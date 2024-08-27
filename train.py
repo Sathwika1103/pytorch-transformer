51e7966f434ddd1ebd4bdd7effdca26474b8b581
Sathwika Gavva <114393608+Sathwika1103@users.noreply.github.com>
2024-08-28 00:23:39 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from model import build_transformer
 from dataset import BilingualDataset, causal_mask
 from config import get_config, get_weights_file_path, latest_weights_file_path
 
-import torchtext.datasets as datasets
+from datasets import load_dataset
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split
@@ -15,7 +15,6 @@ import os
 from pathlib import Path
 
 # Huggingface datasets and tokenizers
-from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
