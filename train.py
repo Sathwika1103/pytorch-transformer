095dc6e3244ca4f4c2ccada6e8a92504fc35e051
Sathwika Gavva <114393608+Sathwika1103@users.noreply.github.com>
2024-08-30 16:27:08 +0530

    Update train.py

--- train.py
+++ train.py
@@ -37,6 +37,33 @@ def prune_model(model, amount=0.5):
             prune.l1_unstructured(module, name='weight', amount=amount)
             prune.remove(module, 'weight')
     return model
+
+def dtype_byte_size(dtype):
+    """
+    Returns the size (in bytes) occupied by one parameter of type `dtype`.
+    """
+    import re
+    if dtype == torch.bool:
+        return 1 / 8
+    bit_search = re.search(r"[^\d](\d+)$", str(dtype))
+    if bit_search is None:
+        raise ValueError(f"`dtype` is not a valid dtype: {dtype}.")
+    bit_size = int(bit_search.groups()[0])
+    return bit_size // 8
+
+def compute_module_sizes(model):
+    """
+    Compute the size of each submodule of a given model.
+    """
+    from collections import defaultdict
+    module_sizes = defaultdict(int)
+    for name, tensor in named_module_tensors(model, recurse=True):
+      size = tensor.numel() * dtype_byte_size(tensor.dtype)
+      name_parts = name.split(".")
+      for idx in range(len(name_parts) + 1):
+        module_sizes[".".join(name_parts[:idx])] += size
+
+    return module_sizes
     
 def get_model_size(model):
     """
@@ -256,12 +283,15 @@ def train_model(config):
 
         # Prune the model after each epoch
         model = prune_model(model, amount=0.5)
-        model = quantize(model, weights=torch.int8, activations=None)
-
         # Print the size of the pruned model
         model_size = get_model_size(model)
         print(f"Model size after pruning and quantisation at epoch {epoch:02d}: {model_size:.2f} MB")
-
+        
+        model = quantize(model, weights=torch.int8)
+        # Print the size of the pruned model
+        module_sizes = compute_module_sizes(model)
+        print(f"The model size is {module_sizes[''] * 1e-9} MB")
+        
         model_filename = get_weights_file_path(config, f"{epoch:02d}")
         torch.save({
             'epoch': epoch,
